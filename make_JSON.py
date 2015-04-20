996613185693ab86731b7509657d2a44fe8d4acd
Panagiotis Koukos <pk3414@bio-iivm1.bio.ic.ac.uk>
2015-04-20 21:20:20 +0100

    restructure the make_JSON.py file to do the gene to og solr formatting as well

--- make_JSON.py
+++ make_JSON.py
@@ -44,26 +44,39 @@ dataset = args.dataset
 key = args.key
 value = args.value
 
-with open(input_file) as in_file:
-    lines = in_file.readlines()
-
-# This holds the info for each cluster.
-cluster_dicts = []
-clustering_dict = {}
 key_col = int(key)
 val_col = int(value)
 
-# Read the keys column to figure out how many clusters there are in the file.
-nof_clusters = len({line.split()[key_col] for line in lines})
+with open(input_file) as in_file:
+    lines = in_file.readlines()
 
-# This holds the info for the clustering.
-if dataset == 'expr':
-    member_type = "gene"
-elif dataset == 'ortho':
-    member_type = "OG"
+# Most of the stuff that follows is only necessary for expr/ortho.
+if dataset == 'mapDict':
+    genes_to_ogs = []
+    with open('plasmo_gene_to_og_mapping', 'w') as out_file:
+        for line in lines:
+            words = line.split()
+            genes_to_ogs.append({words[key_col]: words[val_col]})
+        out_file.write(json.dumps(
+            {
+                "id": "plasmo_genes_to_ogs",
+                "members": genes_to_ogs
+            })
+        )
+# This holds the info for each cluster.
+else:
+    cluster_dicts = []
+    clustering_dict = {}
 
+    # Read the keys column to figure out how many clusters there are in the file.
+    nof_clusters = len({line.split()[key_col] for line in lines})
 
-if dataset == 'expr' or dataset == 'ortho':
+    # This holds the info for the clustering.
+    if dataset == 'expr':
+        member_type = "gene"
+    elif dataset == 'ortho':
+        member_type = "OG"
+
     member_type_dict = {
         "expr": "gene",
         "ortho": "OG"
@@ -75,35 +88,35 @@ if dataset == 'expr' or dataset == 'ortho':
         "member_type": member_type_dict[dataset]
     }
 
-lines_read = 0
-clusters_processed = []
+    lines_read = 0
+    clusters_processed = []
 
-for line in lines:
-    line = line.split()
-    cluster_number = int(line[key_col])
-    if cluster_number not in clusters_processed:
-        clusters_processed.append(cluster_number)
-        cluster_dicts.append(
-            {
-                "id": organism + "_" + dataset + "_cluster_%d_%s" % (nof_clusters, line[key_col]),
-                "type": dataset + "_cluster",
-                "member_ids": [],
-                "name": "Cluster %2d" % (cluster_number + 1),
-                "clustering_id": organism + "_" + dataset + "_cluster_%d" % nof_clusters
-            }
-        )
-        cluster_dicts[-1]['member_ids'].append(line[val_col])
-    else:
-        cluster_dicts[-1]['member_ids'].append(line[val_col])
+    for line in lines:
+        line = line.split()
+        cluster_number = int(line[key_col])
+        if cluster_number not in clusters_processed:
+            clusters_processed.append(cluster_number)
+            cluster_dicts.append(
+                {
+                    "id": organism + "_" + dataset + "_cluster_%d_%s" % (nof_clusters, line[key_col]),
+                    "type": dataset + "_cluster",
+                    "member_ids": [],
+                    "name": "Cluster %2d" % (cluster_number + 1),
+                    "clustering_id": organism + "_" + dataset + "_cluster_%d" % nof_clusters
+                }
+            )
+            cluster_dicts[-1]['member_ids'].append(line[val_col])
+        else:
+            cluster_dicts[-1]['member_ids'].append(line[val_col])
 
-cluster_outfile = "%s_%s_cluster_%02d.json" % (organism, dataset, nof_clusters)
-clustering_outfile = "%s_clustering.json" % organism
+    cluster_outfile = "%s_%s_cluster_%02d.json" % (organism, dataset, nof_clusters)
+    clustering_outfile = "%s_clustering.json" % organism
 
-# Write the clusters json file
-with open(cluster_outfile, 'w') as out_file:
-    out_file.write(json.dumps(cluster_dicts) + '\n')
+    # Write the clusters json file
+    with open(cluster_outfile, 'w') as out_file:
+        out_file.write(json.dumps(cluster_dicts) + '\n')
 
-# and the clustering file as well
-if clustering_dict:
-    with open(clustering_outfile, 'a') as out_file:
-        out_file.write(json.dumps(clustering_dict) + '\n')
+    # and the clustering file as well
+    if clustering_dict:
+        with open(clustering_outfile, 'a') as out_file:
+            out_file.write(json.dumps(clustering_dict) + '\n')
