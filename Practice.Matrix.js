76f83eef79ec2f3303a604c45e14e5e2fcfeb5a9
Thalia <thalia@semplicewebsites.com>
2015-04-21 12:45:24 +0100

    Make selects work, neaten up code

--- Practice.Matrix.js
+++ Practice.Matrix.js
@@ -1,9 +1,11 @@
 /*
-* The matrix of information about the clusters
-*
-* @class
+ * The matrix of information about the clusters
+ *
+ * @class
  */
 Practice.Matrix = function( type1Clusters, type2Clusters, elementDict ) {
+
+	var i, j;
 
 	// An "element" is the basic data unit, e.g. a gene or an orthologous group
 	// A "cluster" contains elements
@@ -23,45 +25,48 @@ Practice.Matrix = function( type1Clusters, type2Clusters, elementDict ) {
 	// sizeMatrix will contain numbers and be used to create the diagram
 	this.elementMatrix = [];
 	this.sizeMatrix = [];
-	for ( var i = 0, ilen = this.matrixSize; i < ilen; i++ ) { // Array for each row
+	for ( i = 0, ilen = this.matrixSize; i < ilen; i++ ) { // Array for each row
 		this.elementMatrix[i] = [];
 		this.sizeMatrix[i] = [];
-		for ( var j = 0, jlen = this.matrixSize; j < jlen; j++ ) { // Array for each col
+		for ( j = 0, jlen = this.matrixSize; j < jlen; j++ ) { // Array for each col
 			this.elementMatrix[i][j] = []; // Will be populated with elements
 		}
 	}
 };
 
 /*
-* Create the elements
+ * Create the elements
  */
 Practice.Matrix.prototype.makeElements = function() {
-	// Reverse dictionary of type 2 clusters and elements
-	var type2ElementToType2Cluster = {};
-	for ( var i = 0, ilen = this.type2Clusters.length; i < ilen; i++ ) {
-		var clusterMembers = this.type2Clusters[i].member_ids;
-		for ( var j = 0, jlen = clusterMembers.length; j < jlen; j++ ) {
+
+	var i, j, clusterMembers, type1Name, type2Name, type2Coo, anElement,
+		// Reverse dictionary of type 2 clusters and elements
+		type2ElementToType2Cluster = {};
+
+	for ( i = 0, ilen = this.type2Clusters.length; i < ilen; i++ ) {
+		clusterMembers = this.type2Clusters[i].member_ids;
+		for ( j = 0, jlen = clusterMembers.length; j < jlen; j++ ) {
 			type2ElementToType2Cluster[clusterMembers[j]] = i;
 		}
 	}
+
 	// Create new elements and first give them type 1 names and type 1 coordinates
 	type2NameUndefinedCount = 0;
-	for ( var i = 0, ilen = this.type1Clusters.length; i < ilen; i++ ) {
-		var clusterMembers = this.type1Clusters[i].member_ids;
+	for ( i = 0, ilen = this.type1Clusters.length; i < ilen; i++ ) {
+		clusterMembers = this.type1Clusters[i].member_ids;
 		console.log( 'Cluster ' + i + ' has ' + clusterMembers.length + ' elements' );
-		for ( var j = 0, jlen = clusterMembers.length; j < jlen; j++ ) {
+		for ( j = 0, jlen = clusterMembers.length; j < jlen; j++ ) {
+
 			// Set up variables for making new elements
-			var type1Name = clusterMembers[j];
-			var type2Name = this.type1ElementToType2Element[type1Name]; // TODO sort this out!
-			if ( type2Name === undefined ) {
-				type2NameUndefinedCount += 1;
-			}
-			var type2Coo = type2ElementToType2Cluster[type2Name];
+			type1Name = clusterMembers[j];
+			type2Name = this.type1ElementToType2Element[type1Name]; // TODO sort this out!
+			type2Coo = type2ElementToType2Cluster[type2Name];
+
 			// Ignore any type 1 elements that do not map to type 2 elements
 			// (Type 2 elements that do not map to type 1 elements are automatically ignored
 			// because type 2 elements are never iterated over)
 			if ( type2Name !== undefined && type2Coo !== undefined ) {
-				var anElement = new Practice.Element(
+				anElement = new Practice.Element(
 					type1Name,
 					type2Name,
 					i, // Type 1 coordinate
@@ -69,44 +74,51 @@ Practice.Matrix.prototype.makeElements = function() {
 				);
 				this.allElements.push( anElement );
 			}
+
 		}
 	}
-	console.log( 'No. type1 names not in dictionary: ' + type2NameUndefinedCount );
+
 };
 
 /*
-* Create the matrix of elements
-*
-* E.g. matrix for 3 type 1 clusters and 2 type 2 clusters:
-*
-* 0 0 X X X
-* 0 0 X X X
-* X X 0 0 0
-* X X 0 0 0
-* X X 0 0 0
-*
+ * Create the matrix of elements
+ *
+ * E.g. matrix for 3 type 1 clusters and 2 type 2 clusters:
+ *
+ * 0 0 X X X
+ * 0 0 X X X
+ * X X 0 0 0
+ * X X 0 0 0
+ * X X 0 0 0
+ *
  */
 Practice.Matrix.prototype.makeElementMatrix = function() {
-	for ( var i = 0, ilen = this.allElements.length; i < ilen; i++ ) {
+
+	var i;
+
+	for ( i = 0, ilen = this.allElements.length; i < ilen; i++ ) {
 		this.elementMatrix[+this.allElements[i].type2Coo][+this.allElements[i].type1Coo + this.numType2Clusters].push( this.allElements[i] );
 		this.elementMatrix[+this.allElements[i].type1Coo + this.numType2Clusters][+this.allElements[i].type2Coo].push( this.allElements[i] );
 	}
 };
 
 /*
-* Create the size matrix
+ * Create the size matrix
  */
 Practice.Matrix.prototype.makeSizeMatrix = function() {
-	for ( var i = 0, ilen = this.matrixSize; i < ilen; i++ ) {
-		for ( var j = 0, jlen = this.matrixSize; j < jlen; j++ ) {
+
+	var i, j, matrixSum;
+
+	for ( i = 0, ilen = this.matrixSize; i < ilen; i++ ) {
+		for ( j = 0, jlen = this.matrixSize; j < jlen; j++ ) {
 			this.sizeMatrix[i][j] = this.elementMatrix[i][j].length;
 		}
 		// Check matrix looks right
 		console.log( this.sizeMatrix[i].toString() );
 	}
-	var matrixSum = 0;
-	for ( var i = 0, ilen = this.matrixSize; i < ilen; i++ ) {
-		for ( var j = 0, jlen = this.matrixSize; j < jlen; j++ ) {
+	matrixSum = 0;
+	for ( i = 0, ilen = this.matrixSize; i < ilen; i++ ) {
+		for ( j = 0, jlen = this.matrixSize; j < jlen; j++ ) {
 			matrixSum += this.sizeMatrix[i][j];
 		}
 	}
@@ -115,7 +127,7 @@ Practice.Matrix.prototype.makeSizeMatrix = function() {
 };
 
 /*
-* Draw the circos diagram
+ * Draw the circos diagram
  */
 Practice.Matrix.prototype.drawCircos = function() {
 
@@ -130,14 +142,14 @@ Practice.Matrix.prototype.drawCircos = function() {
     	}
 	}
 
-	// Adapted from http://bl.ocks.org/mbostock/4062006
+	// The following is adapted from http://bl.ocks.org/mbostock/4062006
 	var chord = d3.layout.chord()
 	    .padding(.05)
 	    .sortSubgroups(d3.descending)
 	    .matrix(this.sizeMatrix);
 
-	var width = 500,
-	    height = 500,
+	var width = 300,
+	    height = 300,
 	    innerRadius = Math.min(width, height) * .41,
 	    outerRadius = innerRadius * 1.1;
 
--- practice.js
+++ practice.js
@@ -1,45 +1,122 @@
-var clusterData = '',
-	expr = '',
-	evol = '';
+// To populate the selects
+var responseData = {},
+	exprOptions = {},
+	orthoOptions = {};
 
-var exprData = {
-	'q'		: 'clustering_id:expr_cluster_5',
+var exprOptionsData = {
+	'q'		: 'type:expr_cluster',
 	'wt'	: 'json',
 	'indent': 'true',
-	'rows' 	: '20'
+	'rows' 	: '200'
 };
 
-var orthoData = {
-	'q'		: 'clustering_id:ortho_cluster_5',
+var orthoOptionsData = {
+	'q'		: 'type:ortho_cluster',
 	'wt'	: 'json',
 	'indent': 'true',
-	'rows' 	: '20'
-};
+	'rows' 	: '200'
+}
 
-var promise1 = $.ajax( "solr.php", {
-	method: "POST",
-	data: exprData,
+var optionsPromise1 = $.ajax( 'solr.php', {
+	method: 'POST',
+	data: exprOptionsData,
 	success: function( response ) {
-		clusterData = response;
-		expr = clusterData.response.docs;
+		var responseData = response.response.docs;
+		for ( var i = 0, ilen = responseData.length; i < ilen; i++ ) {
+			exprOptions[responseData[i].clustering_id[0]] = true;
+		}
+		for ( var key in exprOptions ) {
+			if ( exprOptions.hasOwnProperty( key ) ) {
+				var option = $( '<option>' ).text( key );
+				$( '.expr-cluster-select' ).append( option );
+			}
+		}
 	}
 } );
 
-var promise2 = $.ajax( "solr.php", {
-	method: "POST",
-	data: orthoData,
+var optionsPromise2 = $.ajax( 'solr.php', {
+	method: 'POST',
+	data: orthoOptionsData,
 	success: function( response ) {
-		clusterData = response;
-		evol = clusterData.response.docs;
+		var responseData = response.response.docs;
+		for ( var i = 0, ilen = responseData.length; i < ilen; i++ ) {
+			orthoOptions[responseData[i].clustering_id[0]] = true;
+		}
+		for ( var key in orthoOptions ) {
+			if ( orthoOptions.hasOwnProperty( key ) ) {
+				var option = $( '<option>' ).text( key );
+				$( '.ortho-cluster-select' ).append( option );
+			}
+		}
 	}
 } );
 
-$.when( promise1, promise2 ).done( function( promise1Args, promise2Args ) {
-	var	expr = promise1Args[0].response.docs,
-		evol = promise2Args[0].response.docs;
-	m = new Practice.Matrix( expr, evol, mapDict );
-	m.makeElements();
-	m.makeElementMatrix();
-	m.makeSizeMatrix();
-	m.drawCircos();
+// Once the selects have been populated
+$.when( optionsPromise1, optionsPromise2 ).done( function() {
+	//var response1 = v1[0];
+	$( '.btn-drawCircos' )
+		.removeClass( 'btn-disabled' )
+		.on( 'click', function() {
+			var exprOption = $( '.expr-cluster-select' ).val();
+			var orthoOption = $( '.ortho-cluster-select' ).val();
+			getData( exprOption, orthoOption );
+		} );
 });
+
+function getData( exprOption, orthoOption ) {
+
+	var	clusterData = '',
+		expr = '',
+		ortho = '';
+
+	// function getPromiseFromOption( options ) {
+	// 	var data = {
+	// 		q:option
+	// 	};
+
+	// 	retrun $.ajxj( solr, data, succes)
+	// }
+
+	var exprData = {
+		'q'		: 'clustering_id:' + exprOption,
+		'wt'	: 'json',
+		'indent': 'true',
+		'rows' 	: '20'
+	};
+
+	var orthoData = {
+		'q'		: 'clustering_id:' + orthoOption,
+		'wt'	: 'json',
+		'indent': 'true',
+		'rows' 	: '20'
+	};
+
+	var promise1 = $.ajax( "solr.php", {
+		method: "POST",
+		data: exprData,
+		success: function( response ) {
+			clusterData = response;
+			expr = clusterData.response.docs;
+		}
+	} );
+
+	var promise2 = $.ajax( "solr.php", {
+		method: "POST",
+		data: orthoData,
+		success: function( response ) {
+			clusterData = response;
+			ortho = clusterData.response.docs;
+		}
+	} );
+
+	$.when( promise1, promise2 ).done( function( promise1Args, promise2Args ) {
+		var	expr = promise1Args[0].response.docs,
+			ortho = promise2Args[0].response.docs,
+			m = new Practice.Matrix( expr, ortho, mapDict );
+
+		m.makeElements();
+		m.makeElementMatrix();
+		m.makeSizeMatrix();
+		m.drawCircos();
+	});
+}
