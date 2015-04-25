1f0f5eab6719b3243d87936368ed7710c1a78394
Thalia <thalia@semplicewebsites.com>
2015-04-25 02:23:15 +0100

    Populate dropdowns with options from one species only

--- practice.js
+++ practice.js
@@ -1,3 +1,14 @@
+var selectedSpecies = 'anoph',
+	anophelesOptions = {
+		'expr': {},
+		'ortho': {}
+	},
+
+	plasmodiumOptions = {
+		'expr': {},
+		'ortho': {}
+	};
+
 function getData( field, value ) {
 
 	var data = {
@@ -20,19 +31,69 @@ var optionsPromise2 = getData( 'type', 'ortho_cluster' );
 
 $.when( optionsPromise1, optionsPromise2 ).done( function( v1, v2 ) {
 
-	var i, key, options, option, chosenExpressionOption, chosenOrthoOption, speciesDict,
+	var chosenExpressionOption, chosenOrthoOption, // TODO Move these down
 		expressionOptions = v1[0].response.docs,
 		orthologyOptions = v2[0].response.docs;
 
-	function populateSelect( data, selectClass ) {
-		options = {};
-		for ( i = 0, ilen = data.length; i < ilen; i++ ) {
-			options[data[i].clustering_id] = true;
+	function populateOptions( data ) {
+
+		var i, idList, idSpecies, idType, idNumClusters;
+
+		function addToOptions( species, type, num ) {
+
+			var dict;
+
+			switch ( species ) {
+				case 'anoph':
+					dict = anophelesOptions;
+					break;
+				case 'plasmo':
+					dict = plasmodiumOptions;
+					break;
+			}
+
+			if ( !dict[type][num] ) {
+				dict[type][num] = 1;
+			} else {
+				dict[type][num] += 1;
+			}
+
 		}
-		for ( key in options ) {
-			if ( options.hasOwnProperty( key ) ) {
-				option = $( '<option>' ).text( key );
-				$( '.' + selectClass ).append( option );
+
+		for ( i = 0, ilen = data.length; i < ilen; i++ ) {
+			// WARNING!
+			// The following relies on clutering_id being of the form: species_type_cluster_numClusters
+			idList = data[i].clustering_id.split( '_' );
+			idSpecies = idList[0];
+			idType = idList[1];
+			idNumClusters = idList[3];
+			addToOptions( idSpecies, idType, idNumClusters );
+		}
+
+	}
+
+	function showOptions( species ) {
+
+		var i, key,
+			dictLength,
+			option,
+			type,
+			types = ['expr', 'ortho'];
+
+		switch ( species ) {
+			case 'anoph':
+				dict = anophelesOptions;
+				break;
+			case 'plasmo':
+				dict = plasmodiumOptions;
+				break;
+		}
+
+		for ( i = 0; i < types.length; i++ ) {
+			type = types[i];
+			for ( key in dict[type] ) {
+				option = $( '<option>' ).text( dict[type][key] );
+				$( '.' + type + '-cluster-select' ).append( option );
 			}
 		}
 	}
@@ -42,11 +103,11 @@ $.when( optionsPromise1, optionsPromise2 ).done( function( v1, v2 ) {
 		var promise1 = getData( 'clustering_id', chosenExpressionOption );
 		var promise2 = getData( 'clustering_id', chosenOrthoOption );
 
-		$.when( promise1, promise2 ).done( function( v1, v2 ) {
+		$.when( promise1, promise2 ).done( function( v1i, v2i ) {
 
-			var	expr = v1[0].response.docs,
-				ortho = v2[0].response.docs,
-				m = new Practice.Matrix( expr, ortho, dict ); // TODO sort out mapDict
+			var	expr = v1i[0].response.docs,
+				ortho = v2i[0].response.docs,
+				m = new Practice.Matrix( expr, ortho, dict );
 				console.log( m );
 			m.drawCircos();
 
@@ -54,16 +115,19 @@ $.when( optionsPromise1, optionsPromise2 ).done( function( v1, v2 ) {
 
 	}
 
-	populateSelect( expressionOptions, 'expr-cluster-select' );
-	populateSelect( orthologyOptions, 'ortho-cluster-select' );
+	populateOptions( expressionOptions );
+	populateOptions( orthologyOptions );
+
+	// First show default species options
+	showOptions( selectedSpecies );
 
 	$( '.btn-drawCircos' )
 		.removeClass( 'btn-disabled' )
 		.on( 'click', function() {
 
-			chosenExpressionOption = $( '.expr-cluster-select' ).val();
-			chosenOrthoOption = $( '.ortho-cluster-select' ).val();
-			makeCircos( chosenExpressionOption, chosenOrthoOption, geneToOG );
+			chosenExpressionOption = selectedSpecies + '_expr_cluster_' + $( '.expr-cluster-select' ).val();
+			chosenOrthoOption = selectedSpecies + '_ortho_cluster_' + $( '.ortho-cluster-select' ).val();
+			makeCircos( chosenExpressionOption, chosenOrthoOption, geneToOG ); // TODO sort out global geneToOG
 
 		} );
 
