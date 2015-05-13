e601a6024f93915a584ea0de36cb3249cc6b72c2
Thalia <thalia@semplicewebsites.com>
2015-05-13 23:44:06 +0100

    More complete version to check

--- nodeGoEnrichment.js
+++ nodeGoEnrichment.js
@@ -1,34 +1,51 @@
 var Big = require( './lib/js/big.js-master/big' );
-// Perform GO term enrichment analysis
 
-// Hypergeometric distribution
+// Perform GO term enrichment analysis
 
 // Helper functions
 
-function calculateStirling( n ) {
-	// Stirling's approximation for n!
-	// n! = ((2(pi)n)^1/2) * (e/n)^n
-	// WARNING!
-	// For large n, parts of this may become Infinity
-	// console.log( 2 * Math.PI * n );
-	// console.log( Math.pow( ( 2 * Math.PI * n ), 0.5 ) );
-	// console.log( n / Math.E );
-	// console.log( Math.pow( ( n / Math.E ), n ) );
-	x = n / Math.E;
-	console.log( Big( n / Math.E ).pow( n ) );
-	return Math.pow( ( 2 * Math.PI * n ), 0.5 ) * Math.pow( ( n / Math.E ), n );
-}
+var logFactorial = function( n ) {
+	// Stirling's approximation: log(n!) ~= nlog(n)
 
-function calculateBinomial( n, r ) {
+	// Special case: 	0! = 1
+	// 					log(0!) = 0
+	if ( n === 0 ) {
+		return 0;
+	}
+	x = n * Math.log( n );
+
+	console.log( 'log of ' + n + ' factorial is:' );
+	console.log( x );
+	return x;
+};
+
+var logBinomial = function( n, r ) {
 	// Formula for the binomial coefficient:
-	// nCr = n!/(r!*(n-r)!)
-	return calculateStirling( n ) / ( calculateStirling( r ) * calculateStirling( n - r ) );
-}
+	// nCr = n! / (r! * (n-r)!)
+	// i.e. log(nCr) = log(n! / (r! * (n-r)!))
+	// i.e. log(nCr) = log(n!) - (log(r!) + log((n-r)!))
+	x = logFactorial( n ) - ( logFactorial( r ) + logFactorial( n - r ) );
+
+	console.log( 'log of ' + n + 'C' + r + ' is' );
+	console.log( x );
+	return x;
+};
 
 // Hypergeometric distribution
-// p(X = k) = (KCk)((N-K)C(n-k) / NCn
-// N population size
+// p(X = k) = (KCk)((N-K)C(n-k)) / NCn
+// K number of GO term of interest for Anopheles
+// k number of GO term of interest in cluster of interest
+// N population size (total number GO terms for Anopheles gambiae)
+// n number of GO terms in cluster of interest
 
-//console.log( calculateBinomial( 500, 1 ) );
+module.exports.logHypergeometric = function( K, k, N, n ) {
+	// log(p(X = k)) = log(KCk) + log((N - K)C(n - k)) - log(NCn)
+	x = logBinomial( K, k ) + logBinomial( N - K, n - k ) - logBinomial( N, n );
+	console.log( 'log of hypergeometric is' );
+	console.log( x );
+	return x;
+};
 
-calculateStirling( 10000 );
+module.exports.logHypergeometric( 1, 1, 10000, 5000 );
+
+// var c = require( './nodeGoEnrichment' );
